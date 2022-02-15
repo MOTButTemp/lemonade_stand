43076eab362585749bc3e82751fc93e81cc8e17e
MOTButTemp <91564410+MOTButTemp@users.noreply.github.com>
2022-02-15 14:27:44 -0600

    Update lemonade_stand.py

--- lemonade_stand.py
+++ lemonade_stand.py
@@ -1,124 +1,46 @@
-#!/usr/bin/env python
-
-""" Simple version of classic lemonade stand game. Written in Python 2.7 
-"""
-
-__author__ = 'jeremy osborn'
-
 import random
-
-
-class LemonadeStand:
-
-    """ LemonadeStand class with three methods - make_lemonade, sell_lemonade, display_data.
-    """
-
-    def __init__(self):
-        """ setup initial parameters. weather is randomized."""
-        self.day = 0
-        self.cash = 100
-        self.lemonade = 0
-        self.lemonade_price = random.randrange(1, 10)
-        self.weather = random.randrange(50, 100)
-
-    def make_lemonade(self):
-        """ Make lemonade to sell later. Cost to make lemonade changes each day."""
-        while True:
-            try:
-                lemonade = int(raw_input('How many cups of lemonade will you make (1-10)? '))
-                if lemonade in range(1, 11, 1):
-                    break
-                else:
-                    print('Please choose a number between 1 and 10.')
-                    continue
-            except ValueError:
-                print ('Please choose a number between 1-10.')
-                continue
-        self.lemonade += lemonade
-        self.cash -= lemonade * (float(self.lemonade_price) / 100)
-        self.day += 1
-        self.weather = random.randrange(50, 100)
-        self.lemonade_price = random.randrange(1, 10)
-        print('You made ' + str(lemonade) + ' cups of lemonade!\n')
-
-    def sell_lemonade(self):
-        """ Sell lemonade that you have made previously. Bad weather and/or high price will discount net demand. """
-        while True:
-            try:
-                price = int(raw_input('How many cents will you charge for a cup of lemonade? (0-100) '))
-                if price in range(0, 101):
-                    break
-                else:
-                    print('Please choose a number between 1 and 100')
-                    continue
-            except ValueError, e:
-                print ('Please choose a number between 1 and 100.')
-                continue
-        cups = random.randrange(1, 101)  # without heat or price factors, will sell 1-100 cups per day
-        price_factor = float(100 - price) / 100  # 10% less demand for each ten cent price increase
-        heat_factor = 1 - (((100 - self.weather) * 2) / float(100))  # 20% less demand for each 10 degrees below 100
-        if price == 0:
-            self.lemonade = 0  # If you set price to zero, all your lemonade sells, for nothing.
-            print('All of your lemonade sold for nothing because you set the price to zero.')
-            self.day += 1
-            self.weather = random.randrange(50, 100)
-            self.lemonade_price = random.randrange(1, 10)
-        demand = int(round(cups * price_factor * heat_factor))
-        if demand > self.lemonade:
-            print(
-                'You only have ' + str(self.lemonade) + ' cups of lemonade, but there was demand for ' + str(
-                    demand) + '.')
-            demand = self.lemonade
-        revenue = demand * round((float(price) / 100), 2)
-        self.lemonade -= demand
-        self.cash += revenue
-        self.day += 1
-        self.weather = random.randrange(50, 100)
-        self.lemonade_price = random.randrange(1, 10)
-        print('You sold ' + str(demand) + ' cup(s) of lemonade and earned $' + str(revenue) + ' dollars!\n')
-
-    def display_data(self, name):
-        """ Display all data for the lemonade stand."""
-        if self.day == 0:
-            print('\nWelcome ' + name + '!\n')
-        print('Day: ' + str(self.day))
-        print('Weather: ' + str(self.weather))
-        print('Cash: $' + str(self.cash))
-        print('Lemonade: ' + str(self.lemonade))
-        print('Cost to make Lemonade: $' + str(float(self.lemonade_price) / 100))
-        print('============================' + '\n')
-
-
-def main():
-
-    """ Create new LemonadeStand object and play game, or exit.
-    """
-
-    choice = ''
-    while choice not in ['y', 'n']:
-        choice = raw_input('Create a new lemonade stand? (y/n) ')
-        if choice == 'y':
-            name = raw_input('Hi friend. What is your name? ')
-            stand = LemonadeStand()
-            stand.display_data(name)
-            while True:
-                choice = raw_input('Enter 1 to make lemonade. Enter 2 to sell lemonade. 3 to quit. ')
-                if choice == '1':
-                    stand.make_lemonade()
-                    stand.display_data(name)
-                    continue
-                elif choice == '2':
-                    stand.sell_lemonade()
-                    stand.display_data(name)
-                    continue
-                elif choice == '3':
-                    break
-                else:
-                    print('You must choose either 1 or 2 or 3.')
-                    continue
-        elif choice == 'n':
-            print('Goodbye!')
-            return
-
-
-main()
+import lemonade
+day_count = 1
+money = 100.00
+while day_count <= 7:
+    weather = ""
+    rand_w = random.randint(1,4)
+    if rand_w == 1:
+        weather = "hot"
+    elif rand_w == 2:
+        weather = "sunny"
+    elif rand_w == 3:
+        weather = "overcast"
+    elif rand_w == 4:
+        weather = "cold"
+    print("day" + str(day_count))
+    print("the weather is: " + weather)
+    print("you have $" + str(money) + " total")
+    print()
+    glasses = int(input("how many glasses of lemonade will you make? ($.50/glass) "))
+    cost = float(input("how much will you charge per glass? (1$ - 5$) "))
+    ads = int(input("how many ads will you place? (5/ad) "))
+    glasses_cost = (.5 * glasses)
+    ads_cost = (5 *ads)
+    if money >= glasses_cost:
+              money -= glasses_cost
+              money -= glasses_cost
+    else:
+              glasses = int(money/.5)
+              money -= (glasses * .5)
+              print("You only had enough moeny for " + str(glasses) + " glasses")
+    if money >= ads_cost:
+              money -= ads_cost
+    else:
+              ads = int(money/5)
+              money -= (ads *5)
+              print("You only had enough for " + str(ads) + " ads")
+    sold = lemonade.sell_lemonade(rand_w, glasses, cost, ads)
+    profit = sold * cost
+    money += profit
+    print("You sold " + str(sold) + " glasses of lemonade today and made $" +str(profit))
+    print()
+    day_count += 1
+print("It's been a week! You earned your time with $" + str(money))
+print("That's a total profit of $" + str(money - 100.0))
+    
